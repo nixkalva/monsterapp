614fed1546d16e670a78df802edf7db3b173ae0a
Nikhila Kalva <nixkalva@gmail.com>
2019-10-01 07:31:41 +1000

    optimized
    simple sample

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './Components/card-list/card-list-component.jsx';
-
+import { SearchBox } from './Components/search-box/search-box.component.jsx';
 import './App.css';
 
 class App extends Component {
@@ -16,17 +16,24 @@ class App extends Component {
     .then(response => response.json())
     .then(users =>  this.setState({monsters: users}));
   }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value });
+  };
+
+  
   render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
         <h1> Nikhila </h1>
         <h1> Hi Nik you are Awesome </h1>
-        <input 
-          type="search" 
+        <SearchBox 
           placeholder='search monsters'
-          onChange={ e => this.setState({ searchField: e.target.value})}
+          handleChange= {this.handleChange}
           />
-        <CardList monsters= {this.state.monsters} />
+        <CardList monsters= {filteredMonsters} />
               
       </div>
     );
